1ab2a79fff6199525e92b4c2160393ddd9ca843f
nibutaniSNK <m.werber@sbcglobal.net>
2021-07-02 22:01:40 -0500

    configured firebase hosting
    site can now be viewed by anyone at https://tfh-webapp.web.app/#/

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tfh-webapp
 Repository for the TFH Webapp
+
+## local testing
+`npm run serve`
+
+## firebase deployment
+`firebase login` to make sure you're logged in to firebase
+`npm run build` to build dist folder
+`firebase deploy` to deploy
+https://tfh-webapp.web.app/#/
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 Vue.use(Vuetify)
 
 export default new Vuetify({
     rtl: false,
+    icons: {
+      iconfont: 'mdi'
+    },
     theme: {
       dark: true,
-      opions: {customProperties: true},
+      options: {customProperties: true},
       themes: {
         dark: {
           background: '#4b4b4b',
